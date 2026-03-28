1190ecabdf031692a4e4e95f4e913e4290814b86
yaroslav228 <kibersportstandoff69@gmail.com>
2026-03-28 15:33:15 +0200

    fixed 1st task

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function makeTransaction(quantity ,pricePerDroid){
- return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`
+ return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
 }
 
 
